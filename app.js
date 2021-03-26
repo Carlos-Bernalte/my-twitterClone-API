12f020f7b7ae3c17d8aff69b57e441352d1a2138
Carlos-Bernalte <carlos.bernalte@outlook.com>
2021-03-26 20:31:55 +0100

    A ver si asi funciona

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
 
 var indexRouter = require('./routes/index');
 var postsRouter = require('./routes/posts');
@@ -13,17 +10,29 @@ var postsRouter = require('./routes/posts');
 var bodyParser  = require("body-parser");   //nuevo
 var cors = require('cors');   //nuevo
 
-var app = express();
-
 require('dotenv').config();
+
+var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then(() =>  console.log('mymerndb connection successful'))
+    .catch((err) => console.error(err));
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());  //nuevo
+app.use(bodyParser.json({limit: '50mb'}));  //nuevo
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));   //nuevo
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -34,14 +43,6 @@ app.use('/posts', postsRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-app.use(cors());  //nuevo
-app.use(bodyParser.json({limit: '50mb'}));  //nuevo
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));   //nuevo
-
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
-    .then(() =>  console.log('mymerndb connection successful'))
-    .catch((err) => console.error(err));
 
 // error handler
 app.use(function(err, req, res, next) {
